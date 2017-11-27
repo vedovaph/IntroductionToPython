a1bb5d9829cfbdc5d33dc95d8bfe0ad72575d482
vedovaph <vedovaph@rose-hulman.edu>
2017-11-27 14:01:45 -0500

    Completed all todos

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Patrick Vedova.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,28 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+pacman = rg.SimpleTurtle('turtle')
+pacman.pen = rg.Pen('yellow',10)
+blinkie = rg.SimpleTurtle('turtle')
+blinkie.pen = rg.Pen('blue',5)
+pacman.speed = 5
+blinkie.speed = 4
+size = 100
+for n in range(20):
+    pacman.draw_regular_polygon(8, 50)
+    pacman.pen_up()
+    pacman.right(45)
+    pacman.forward(10)
+    pacman.left(45)
+    pacman.pen_down()
+    blinkie.draw_regular_polygon(7, 50)
+    blinkie.pen_up()
+    blinkie.right(45)
+    blinkie.forward(25)
+    blinkie.left(45)
+    blinkie.pen_down()
+
+
+
